3fdebae63659c927172b4667ad8fe43a75e93d4e
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-03-15 00:57:39 +0000

    tests did not go good

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -1,5 +1,15 @@
-def test_run
-  subshop1 = Subshop.new
-  assert_equal , subshop1.run, "Run method failed"
+require "minitest/autorun"
+require "./subshop_app"
+
+class TestSubshop < Minitest::Test
+  def test_run
+    subshop1 = Subshop.new
+    assert_equal nil, subshop1.run, "Run method failed"
+  end
+  def test_order_return
+    subshop1 = Subshop.new
+    assert_equal ["Ham", "No Cheese", "No Veggies", "Drink: Coke"], subshop1.run, "Run method failed"
+  end
 end
-end
+
+#type "ruby tests/test_calculator.rb" to run test
