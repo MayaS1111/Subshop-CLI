959bec911561aacdcf1cc81da83b75e647ea0ea0
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-03-15 00:58:13 +0000

    updated test

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -6,10 +6,6 @@ class TestSubshop < Minitest::Test
     subshop1 = Subshop.new
     assert_equal nil, subshop1.run, "Run method failed"
   end
-  def test_order_return
-    subshop1 = Subshop.new
-    assert_equal ["Ham", "No Cheese", "No Veggies", "Drink: Coke"], subshop1.run, "Run method failed"
-  end
 end
 
 #type "ruby tests/test_calculator.rb" to run test
